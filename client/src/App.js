25d895c7edf02f99dc3c91a52587de3d4273e059
Mike Armstrong <mjarmstr@yahoo.com>
2022-09-19 21:58:23 -0500

    Completed Activity 4

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
-  renter() {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000')
+      .then((response) => {
+        this.setState({
+          data: response.data
+        })
+      })
+      .catch((error) => {
+        console.error(`Error fetching data: ${error}`)
+      })
+  }
+
+  render() {
     return (
       <div className="App">
         <header className="App-header">
